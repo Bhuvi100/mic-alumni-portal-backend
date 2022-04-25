e72a02b78621703682b207ef326e040202dfbc65
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-04-25 19:28:48 +0530

    Update email Styling with inline styles

--- resources/views/emails/magic-login.blade.php
+++ resources/views/emails/magic-login.blade.php
@@ -1,8 +1,3 @@
-<style>
-    .btn{display:inline-block;font-weight:400;line-height:1.5;color:#212529;text-align:center;text-decoration:none;vertical-align:middle;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;user-select:none;background-color:transparent;border:1px solid transparent;padding:.375rem .75rem;font-size:1rem;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out}@media (prefers-reduced-motion:reduce){.btn{transition:none}}.btn:hover{color:#212529}.btn-check:focus+.btn,.btn:focus{outline:0;box-shadow:0 0 0 .25rem rgba(13,110,253,.25)}
-    .btn-block{display:block; padding-top:15px; padding-bottom: 15px}.btn-block+.btn-block{margin-top:.5rem}input[type=button].btn-block,input[type=reset].btn-block,input[type=submit].btn-block{width:100%}
-</style>
-
 <p>
     Dear Innovator,<br>
     Warm welcome from Ministry of Education’s Innovation Cell!<br>
@@ -17,7 +12,7 @@
 Share your success stories to showcase in Ministry of Education’s website to inspire the young innovators.
 <p></p>
 <br><br>
-<a href="{{$login_url}}" class="btn btn-block" style="background-color: steelblue;color: white; margin-left: 5%; margin-right: 5%" class="login_button">
+<a href="{{$login_url}}" style="background-color: steelblue;color: white; margin-left: 5%; margin-right: 5%;font-weight:400;line-height:1.5;text-align:center;text-decoration:none;vertical-align:middle;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;user-select:none;border:1px solid transparent;padding:.375rem .75rem;font-size:1rem;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out; display:block; padding-top:15px; padding-bottom: 15px" class="login_button">
     Click here to login in MIC alumni portal</a>
 <br><br><br>
 <p>
